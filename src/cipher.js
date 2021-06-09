1201e035afe40e6ccfe03b2322cdbc648a4f8263
Letícia Braga Aniceto <leticia.aniceto@usp.br>
2021-06-09 15:02:40 -0300

    Correção da funcionalidade de rebater o texto inserido para as caixas de texto de resultado utilizando function.

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,68 @@
+const codigoDaLetraASC = "".charCodeAt(0);
+console.log(codigoDaLetraASC);
+const tamanhoAlfabeto = 26;
+const cod1aLetra = 65;
+//let desloc = Number.("")
+//['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+let alfabeto = "";
+for (let i=65; i<=90; i+=1){
+  alfabeto += String.fromCodePoint
+}
+
+
 const cipher = {
-  // ...
+  encode: function (offset, str) {
+    /* function checkInput (evt) {
+      var charCodeAt = evt.charCodeAt;
+      if (charCodeAt != 0){
+        if (charCodeAt < 65 || charCodeAt > 90){
+          evt.preventDefault();
+          displayWarning(
+            "Por favor utilize apenas letras maiúsculas." + "\n" + "charCodeAt:" + charCodeAt + "\n"
+          );
+        }
+      }
+    } */
+  console.log("aqui offset", offset); 
+  console.log("aqui texto", str); 
+  } 
+
+  
+
 };
 
 export default cipher;
+
+/* usar código ASC 
+"A".charCodeAt --> descobrir o código da letra/posição 
+
+alfabeto começando A=0:
+(codigoDaLetra + desloc) % tamDoAlfabeto
+    F=5      +      7    % 26
+
+
+alfabeto começando A=65 (código ASC):
+A    B   (...) Z
+65  66         90
+
+((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
+1aLetra = A = 65 ("A".charCodeAt(0))
+
+"F".charCodeAt(0) -65
+return: 5
+"F".charCodeAt(0)
+70
+
+((codigoDaLetraASC - cod1aLetra + desloc) % tamDoAlfabeto) + cod1aLetra
+((70 -65 + 7) % 26) + 65
+
+
+String.fromCharCode(72)
+return: "H"
+
+function cifra(string) {
+  return ???
+}
+*/
+
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-
-
 import cipher from './cipher.js';
-
 console.log(cipher);
+
+
+
+//let offset = document.getElementById("offset");
+
+
+//inserir texto do input na caixa de resultado:
+  function cifrar (event) {
+    event.preventDefault();
+    let resultadoCif = document.getElementById('resultado-msgcifrada');
+    resultadoCif.innerHTML = document.getElementById('texto-cifrar').value
+    //resultadoCif.innerHTML = document.myform.cifrar.value;
+    console.log("function cifrar()"); 
+  }
+  document.getElementById('button-cifrar').addEventListener("click", cifrar)
+
+  function decifrar (event) {
+    event.preventDefault();  
+    let resultadoDecif = document.getElementById('resultado-msgdecifrada');
+    resultadoDecif.innerHTML = document.getElementById('texto-decifrar').value;
+    console.log("function decifrar()"); 
+  }
+  document.getElementById('button-decifrar').addEventListener("click", decifrar)
+ 
