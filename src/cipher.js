e28fe88d635e50667a784d183dd97232979da106
Letícia Braga Aniceto <leticia.aniceto@usp.br>
2021-06-18 22:04:32 -0300

    Finalização da estética e do HTML da página.

--- src/cipher.js
+++ src/cipher.js
@@ -1,66 +1,96 @@
-//funciona com maiúsculas e minúsculas, porém teste "alo Alo DEV"
-//erro no offset minusculo
+const cipher = {
+  encode: function (offset, str) {
+    if (offset != Number && str == "") {
+      throw TypeError("Por favor preencha os campos");
+    } else {
+      let encMsg = "";
+      for (let i = 0; i < str.length; i++) {
+        let code = str[i].charCodeAt(0);
+        if (code >= 65 && code <= 90) {
+          let resEncode = ((code + 65 + offset) % 26) + 65;
+          encMsg += String.fromCharCode(resEncode);
+        } else if (code >= 97 && code <= 122) {
+          let resEncode = ((code - 97 + offset) % 26) + 97;
+          offset
+          encMsg += String.fromCharCode(resEncode);
+        } else {
+          encMsg += str[i]
+        }
+      }
+      return encMsg;
+    }
+  },
+
+  decode: function (offset, str) {
+    if (offset != Number && str == "") {
+      throw TypeError("Por favor preencha os campos");
+    } else {
+      let decMsg = "";
+      for (let i = 0; i < str.length; i++) {
+        let code = str[i].charCodeAt(0);
+        if (code >= 65 && code <= 90) {
+          let resDecode = ((code + 65 - offset) % 26) + 65;
+          decMsg += String.fromCharCode(resDecode);
+        } else if (code >= 97 && code <= 122) {
+          let resDecode = 122 - ((122 - code + offset) % 26);
+          decMsg += String.fromCharCode(resDecode);
+        } else {
+          decMsg += str[i]
+        }
+      }
+      return decMsg;
+    }
+  }
+}
+
+
+export default cipher;
+
+
+
+
+//security copy
+/*
 const cipher = {
   encode: function (offset, str) {
     let encMsg = "";
 
     for (let i = 0; i < str.length; i++) {
       let code = str[i].charCodeAt(0);
-
       if (code >= 65 && code <= 90) {
-        let resEncode = ((code + 65 + offset) % 26) + 65; 
-        //65 = maiuscula  26 = alfabeto (se for 52 entra em outros caracteres)
+        let resEncode = ((code + 65 + offset) % 26) + 65;
         encMsg += String.fromCharCode(resEncode);
       } else if (code >= 97 && code <= 122) {
-        let resEncode = ((code + 97 + offset) % 26) + 97; //97 = minuscula
+        let resEncode = ((code - 97 + offset) % 26) + 97;
+        offset
         encMsg += String.fromCharCode(resEncode);
       } else {
         encMsg += str[i]
-      } 
+      }
     }
     return encMsg;
   },
 
-
-//funciona com maiúsculas e minúsculas, porém teste "alo Alo DEV"
   decode: function (offset, str) {
     let decMsg = "";
 
     for (let i = 0; i < str.length; i++) {
       let code = str[i].charCodeAt(0);
-
       if (code >= 65 && code <= 90) {
         let resDecode = ((code + 65 - offset) % 26) + 65;
         decMsg += String.fromCharCode(resDecode);
       } else if (code >= 97 && code <= 122) {
-        let resDecode = ((code + 97 - offset) % 26) + 97;
+        let resDecode = 122 - ((122 - code + offset) % 26);
         decMsg += String.fromCharCode(resDecode);
       } else {
         decMsg += str[i]
       }
-  }
+    }
   return decMsg;
   },
 }
 
 export default cipher;
+*/
 
 
-
-  /* essa funciona com maiúsculas (não mexer)
-  encode: function (offset, str) {
-    let encMsg = "";
-
-    for (let i = 0; i < str.length; i++) {
-      let code = str[i].charCodeAt(0);
-
-      if (code >= 65 && code <= 90) {
-        let resEncode = ((code + 65 + offset) % 26) + 65;
-        encMsg += String.fromCharCode(resEncode);
-      } else {
-        encMsg += str[i] 
-      }
-    }
-    return encMsg;
-  },
-  */
--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import cipher from './cipher.js';
-console.log(cipher);
 
-//inserir texto do input na caixa de resultado:
+const btnEncrypt = document.getElementById("btnEncrypt");
+btnEncrypt.onclick = function encrypt(event) {
+  event.preventDefault();
+  let offset = parseInt(document.getElementById("offset").value);
+  let textEncrypt = document.getElementById("textEncrypt").value;
+  document.getElementById("resultEncrypt").innerHTML = cipher.encode(offset, textEncrypt);
+}
 
-btnEncrypt.onclick = function encrypt (event) {
-    event.preventDefault();
-    let offset = parseInt(document.getElementById("offset").value);
-    let textEncrypt = document.getElementById("textEncrypt").value;
-    document.getElementById("resultEncrypt").innerHTML = cipher.encode(offset, textEncrypt);
-  }
- 
+const btnDecrypt = document.getElementById("btnDecrypt");
+btnDecrypt.onclick = function decrypt(event) {
+  event.preventDefault();
+  let offset = parseInt(document.getElementById("offset").value);
+  let textDecrypt = document.getElementById("textDecrypt").value;
+  document.getElementById("resultDecrypt").innerHTML = cipher.decode(offset, textDecrypt);
+}
 
-//inserir texto do input na caixa de resultado: 
-btnDecrypt.onclick = function decrypt (event) {
-    event.preventDefault();  
-    let offset = parseInt(document.getElementById("offset").value);
-    let textDecrypt = document.getElementById("textDecrypt").value;
-    document.getElementById("resultDecrypt").innerHTML = cipher.decode(offset, textDecrypt);
-  }
-  
- 
+
